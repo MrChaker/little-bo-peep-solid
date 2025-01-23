b53db65d484a73879e3a57b3f0868b1823a42814
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-01-23 15:34:47 +0100

    remove leptos_emitter dependency

--- src/components/TOCAuthorSuppliedContent.tsx
+++ src/components/TOCAuthorSuppliedContent.tsx
@@ -5,51 +5,49 @@ import Spacer from "./Spacer";
 const TOCAuthorSuppliedContent = () => {
   return (
     <>
-      <div id="chapter">
-        <TOCTitle label="Chapters" />
+      <div
+           id="chapter">
+        <TOCTitle
+             label="Chapters" />
         <ul>
           <TOCItem
-            article_type={1}
-            label="A Few Refreshers"
-            on_mobile="A Few Refreshers"
-            href="chapter1"
-          />
+               article_type={1}
+               label="A Few Refreshers"
+               on_mobile="A Few Refreshers"
+               href="chapter1" />
           <TOCItem
-            article_type={2}
-            label="Slopes"
-            on_mobile="Slopes"
-            href="chapter2"
-          />
+               article_type={2}
+               label="Slopes"
+               on_mobile="Slopes"
+               href="chapter2" />
           <TOCItem
-            article_type={3}
-            label="Functions"
-            on_mobile="Functions"
-            href="chapter3"
-          />
+               article_type={3}
+               label="Functions"
+               on_mobile="Functions"
+               href="chapter3" />
           <TOCItem
-            article_type={4}
-            label="Derivatives"
-            on_mobile="Derivatives"
-            href="chapter4"
-          />
+               article_type={4}
+               label="Derivatives"
+               on_mobile="Derivatives"
+               href="chapter4" />
         </ul>
       </div>
       <Spacer />
-      <div id="bootcamp">
-        <TOCTitle label="Bootcamps" />
+      <div
+           id="bootcamp">
+        <TOCTitle
+             label="Bootcamps" />
         <ul>
           <TOCItem
-            article_type={1}
-            label="Sets"
-            on_mobile="Sets"
-            href="bootcamp1"
-          />
+               article_type={1}
+               label="Sets"
+               on_mobile="Sets"
+               href="bootcamp1" />
           <TOCItem
-            article_type={2}
-            label="Powers of 10"
-            on_mobile="Powers of 10"
-            href="bootcamp2"
-          />
+               article_type={2}
+               label="Powers of 10"
+               on_mobile="Powers of 10"
+               href="bootcamp2" />
         </ul>
       </div>
     </>
